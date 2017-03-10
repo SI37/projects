b61c6a1c25d514d531d601a5224a41982a77772b
Sudish Itwaru <suditw@gmail.com>
2017-03-10 09:53:38 -0500

    test1

---
+++ example/src/main/java/com/revature/example/App.java
@@ -0,0 +1,14 @@
+package com.revature.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Hello");
+        System.out.println( "Hello World!" );
+    }
+}
